30fcfc60dbe912c5942841e4662ea0f8468f2431
unknown <leonidtatarchenkov@gmail.com>
2024-05-28 22:28:17 +0500

    1

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,4 +1,4 @@
-const allowedCors = ["https://pindie.front.nomoredomainswork.ru"];
+const allowedCors = ["https://frontend-rabota.nomorepartiesco.ru"];
 
 function cors(req, res, next) {
   const { origin } = req.headers;
